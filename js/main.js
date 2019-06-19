fa5497a6e64d972d7225f1488fd4a9e53d3b1e98
Jared Walker <jamjarwalker@gmail.com>
2019-06-19 16:08:08 -0500

    Accessibility
    bug: corrected tabindexes on restaurant and home page

--- js/main.js
+++ js/main.js
@@ -162,7 +162,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.setAttribute('alt', restaurant.name);
+  image.setAttribute('alt', restaurant.name);// add alt text for image
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
@@ -212,4 +212,13 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+//register service worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('js/sw.js')
+  .catch(function (err) {
+    console.error(err);
+  });
+}
 
